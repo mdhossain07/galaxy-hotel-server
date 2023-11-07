e536dd9eaecce860c57e729ee546cc8148907138
mdhossain07 <mrabir8097@gmail.com>
2023-11-07 15:34:28 +0600

    review get api created

--- index.js
+++ index.js
@@ -122,6 +122,23 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/review", async (req, res) => {
+      let query = {};
+      if (req.query?.sid) {
+        query = { sid: req.query.sid };
+      }
+      const cursor = reviewCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewCollection.findOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
