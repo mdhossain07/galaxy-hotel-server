b5e29c6bcd921b13d071f051696e9209e48636b8
mdhossain07 <mrabir8097@gmail.com>
2023-11-07 19:30:14 +0600

    jwt authentication done

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.port || 5001;
 require("dotenv").config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esabfel.mongodb.net/?retryWrites=true&w=majority`;
@@ -18,12 +26,46 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middlewares
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status.send(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     const roomsCollection = client.db("galaxyDB").collection("rooms");
     const bookingCollection = client.db("galaxyDB").collection("booking");
     const reviewCollection = client.db("galaxyDB").collection("review");
     // await client.connect();
+
+    // auth related API
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
 
     // Rooms API
 
@@ -59,7 +101,10 @@ async function run() {
     //   }
     // });
 
-    app.get("/booking", async (req, res) => {
+    app.get("/booking", verifyToken, async (req, res) => {
+      if (req.user?.email !== req.query.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -86,6 +131,29 @@ async function run() {
         console.log(err);
       }
     });
+
+    // app.post("/booking", async (req, res) => {
+    //   const { img, available, checkIn, checkOut, _id } = req.body;
+    //   const intBook = parseInt(available);
+    //   if (intBook > 0) {
+    //     const updateRoomNumber = intBook - 1;
+    //     const result = await bookingCollection.insertOne({
+    //       img,
+    //       available: updateRoomNumber,
+    //       checkIn,
+    //       checkOut,
+    //     });
+    //     const updateRoom = await roomsCollection.updateOne(
+    //       {id: _id},
+    //       {$inc: { intBook: -1 },
+    //     });
+    //     res.send(result);
+    //     res.send(updateRoom);
+    //     console.log("booking successfull");
+    //   } else {
+    //     console.log("unsuccessfull");
+    //   }
+    // });
 
     app.put("/booking/:id", async (req, res) => {
       const newDate = req.body;
@@ -124,7 +192,7 @@ async function run() {
 
     app.get("/review", async (req, res) => {
       let query = {};
-      if (req.query?.sid) {
+      if (req.query.sid) {
         query = { sid: req.query.sid };
       }
       const cursor = reviewCollection.find(query);
